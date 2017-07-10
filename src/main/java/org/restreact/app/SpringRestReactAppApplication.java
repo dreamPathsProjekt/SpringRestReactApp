3be9b321e4e158b16eec067bb8b98d28e32ad657
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-10 18:05:04 +0300

    Initial Commit

---
+++ src/main/java/org/restreact/app/SpringRestReactAppApplication.java
@@ -0,0 +1,12 @@
+package org.restreact.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestReactAppApplication.class, args);
+	}
+}
---
+++ src/test/java/org/restreact/app/SpringRestReactAppApplicationTests.java
@@ -0,0 +1,16 @@
+package org.restreact.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestReactAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
