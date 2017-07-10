5a166948ee216907a9aabd0e9a4be1a446b15008
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-10 23:31:05 +0300

    Add records

--- src/main/java/org/restreact/app/DatabaseLoader.java
+++ src/main/java/org/restreact/app/DatabaseLoader.java
@@ -4,28 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-
 @Component
-public class DatabaseLoader implements CommandLineRunner{
-	
+public class DatabaseLoader implements CommandLineRunner {
+
 	private final EmployeeRepository repository;
 
-	
 	@Autowired
 	public DatabaseLoader(EmployeeRepository repository) {
-		//super();
+		// super();
 		this.repository = repository;
 	}
-
-
 
 	@Override
 	public void run(String... arg0) throws Exception {
 		Employee dummyEmployee = new Employee();
+		Employee dummy2 = new Employee();
 		dummyEmployee.setFirstName("Ioannis");
 		dummyEmployee.setLastName("Dritsas");
-		 dummyEmployee.setDescription("Full-suck developer");
-		this.repository.save(dummyEmployee);		
+		dummyEmployee.setDescription("Full-suck developer");
+		dummy2.setFirstName("Test");
+		dummy2.setLastName("Value");
+		dummy2.setDescription("Full-suck developer too");
+		this.repository.save(dummyEmployee);
+		this.repository.save(dummy2);
 	}
 
 }
